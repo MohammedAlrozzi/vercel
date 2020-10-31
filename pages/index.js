9e7f5d13628741fad981bdbf0d44b1847d1c6f7c
MohammedAlrozzi <25036156+MohammedAlrozzi@users.noreply.github.com>
2020-10-31 18:36:06 +0000

    test3

--- pages/index.js
+++ pages/index.js
@@ -30,36 +30,10 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-        <h1 className={styles.title}>
+        <h3 className={styles.title}>
           Welcome to <a href="https:mohammedalrozzi.github.io/">my page!</a>
-        </h1>
+        </h3>
       </main>
 
       <footer className={styles.footer}>
